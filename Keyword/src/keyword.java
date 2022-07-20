642f902c5508462a3619fcaecf5b50ed04bd50bb
Prathmesh <rafalemultirole94@gmail.com>
2022-07-20 18:26:28 +0530

    Keyword & Literals & Identifier concept in Java

---
+++ Keyword/src/keyword.java
@@ -0,0 +1,33 @@
+/*
+Keyword - keywords are the reserved word whose meaning is already defined in the java compiler.
+
+Identifiers are called name used as variable name like String newIdentifier like this.
+
+null, true, false used as a literals in java. 50 keywords + 3 literals.
+
+Note - 1) We can't use keywords as a variable name, method name.
+2) keywords are the case-sensitive.
+
+Identifiers refer to the name of variables, methods, classes and so on.
+for eg- int a; here a is identifier
+void m1 - m1 is literal
+class A - A is literal
+
+ALL IDENTIFIERS ARE NOT VARIABLE BUT ALL VARIABLE NAME CAN BE IDENTIFIER.
+ */
+
+public class keyword {
+
+    public static void main(String[] args) {
+        System.out.println("Keyword - keywords are the reserved word whose meaning is already defined in the java compiler.");
+        System.out.println("We can't use keywords as a variable name, method name.");
+        System.out.println("Identifiers refer to the name of variables, methods, classes and so on.");
+        int a;
+       // class A;
+
+        System.out.println("ALL IDENTIFIERS ARE NOT VARIABLE BUT ALL VARIABLE NAME CAN BE IDENTIFIER.");
+
+
+
+    }
+}
