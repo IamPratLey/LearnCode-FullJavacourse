3a7e1324903452279c0ed89274f6f8710fa3cea7
Prathmesh <rafalemultirole94@gmail.com>
2022-07-28 12:30:44 +0530

    Resulting data type after arithmetic operation (important)

---
+++ Resulting data type after arithmetic operation/src/resultdatatype.java
@@ -0,0 +1,22 @@
+
+public class resultdatatype {
+
+    public static void main(String[] args) {
+        System.out.println("Resulting data type after arithmetic operation/output data type after operation");
+        System.out.println("Output/Result = byte + short -> integer");
+        System.out.println("Result = short + integer -> integer");
+        System.out.println("Result =  long + float -> float");
+        System.out.println("Result = integer + float -> float");
+        System.out.println("Result = character + integer -> integer");
+        System.out.println("Result = character + short -> integer");
+        System.out.println("Result = long + double -> double");
+        System.out.println("Result = float + double -> double");
+        System.out.println("Quick Quiz: What will be the value of the following expression(x).");
+
+                int y = 7;
+                int x = ++y * 8;
+        System.out.println("Value will be " + x);
+
+
+    }
+}
